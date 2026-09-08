0feacfafe565d7e1cff02475389b5a00aaf702e8
raushansingh2602 <raushansingh5594@gmail.com>
2026-09-09 01:29:10 +0530

    Fix CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,14 +8,9 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="NOVA API")
 
-origins = [
-    "http://localhost:3000",
-    "http://localhost:8000"
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
